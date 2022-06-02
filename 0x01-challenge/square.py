e2b96d50ccfc3a97c1aee0c6e0cc314e8986d721
Matilop15 <3959@holbertonschool.com>
2022-06-02 12:56:59 +0000

    update squeare

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -23,12 +23,12 @@ class Square():
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """ str """
+        """ str formatt"""
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    """ def """
+    """ call all function"""
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
